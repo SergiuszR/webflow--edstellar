a8e5f94c2697517cb3602c04cdd4a56f3eb230a2
SergiuszRozycki <smartbastard@icloud.com>
2022-09-22 14:01:54 +0200

    update

--- main.js
+++ main.js
@@ -23,27 +23,3 @@ function navbarBg() {
 window.addEventListener("load", navbarBg);
 window.addEventListener("scroll", navbarBg);
 window.addEventListener("resize", navbarBg);
-
-window.addEventListener("load", () => {
-  try {
-    const interval = setInterval(function () {
-      let activeTab = document.querySelector(
-        '.tms-tabs-menu > [aria-selected="true"]'
-      );
-      let inactiveTab = document.querySelectorAll(
-        '.tms-tabs-menu > [aria-selected="false"]'
-      );
-
-      let ActiveIcon = activeTab.children[0].children[1];
-      ActiveIcon.src =
-        "https://uploads-ssl.webflow.com/62bcaf4c9767291fa1cb720f/632c440326040b7f3a01ee60_line-1.svg";
-
-      inactiveTab.forEach((n) => {
-        n.children[0].children[1].src =
-          "https://global-uploads.webflow.com/62bcaf4c9767291fa1cb720f/62bcc1942862382d560bab0a_plus.svg";
-      });
-    }, 10);
-  } catch (error) {
-    return;
-  }
-});
